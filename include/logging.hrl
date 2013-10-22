c280ef2bb5a3167b0ef32b1f3d9536e65608173f
stwind <stwindfy@gmail.com>
2013-10-22 12:30:56 +0800

    Remove line break in logging

--- include/logging.hrl
+++ include/logging.hrl
@@ -1,5 +1,5 @@
--define(info(Fmt), error_logger:info_msg(Fmt ++ "~n")).
--define(info(Fmt, Args), error_logger:info_msg(Fmt ++ "~n", Args)).
+-define(info(Fmt), error_logger:info_msg(Fmt)).
+-define(info(Fmt, Args), error_logger:info_msg(Fmt, Args)).
 
--define(error(Fmt), error_logger:error_msg(Fmt ++ "~n")).
--define(error(Fmt, Args), error_logger:error_msg(Fmt ++ "~n", Args)).
+-define(error(Fmt), error_logger:error_msg(Fmt)).
+-define(error(Fmt, Args), error_logger:error_msg(Fmt, Args)).
